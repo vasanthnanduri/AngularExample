e61ea083317375f8f1fcde3288bacc287aeba09d
vasanth95nanduri <36486845+vasanth95nanduri@users.noreply.github.com>
2018-07-08 02:41:06 -0400

    Created Login Component. Edited the app.component.html and app.component.ts to required data. Placed Login component in app.module.ts Imported Routing module and defined route for the login in app module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {RouterModule, Routes} from '@angular/router';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
+const appRoutes: Routes = [
+  {path: 'login', component: LoginComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
